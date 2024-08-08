fb25efbac50e429026e609702d843e27eb8dd0f6
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2024-08-08 13:43:13 -0300

    ➕ DotENV
    A library is installed to use environment variables in the project with their corresponding configuration.

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import {THE_MOVIE_DB_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: 'ac4485972019b9c2f60bbf6cd17d4766',
+    api_key: THE_MOVIE_DB_KEY,
     language: 'es',
   },
 });
